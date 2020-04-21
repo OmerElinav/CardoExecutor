d5f3a021092644792415116e4c26c10cdcac43d3
omer <omerelinav@gmail.com>
2020-04-21 09:27:25 +0300

    generalised _get_attribute parameters

--- CardoExecutor/Common/CardoWrapper.py
+++ CardoExecutor/Common/CardoWrapper.py
@@ -4,14 +4,13 @@ PRIVATE_PREFIX = "_"
 
 def get_wrapped_attribute(cardo_object, item: str, inner_object_name: str = _INNER):
     df = object.__getattribute__(cardo_object, inner_object_name)
-    df_type = type(df)
-    return _get_attribute(cardo_object, df, df_type, item)
+    return _get_attribute(cardo_object, df, type(df), item)
 
 
-def _get_attribute(cardo_object, df, df_type, item):
-    if hasattr(df_type, item):
-        value = object.__getattribute__(df, item)
-        return _wrap(value, cardo_object, df_type) if callable(value) else value
+def _get_attribute(cardo_object, inner, inner_type, item: str):
+    if hasattr(inner_type, item):
+        value = object.__getattribute__(inner, item)
+        return _wrap(value, cardo_object, inner_type) if callable(value) else value
     return object.__getattribute__(cardo_object, item)
 
 
